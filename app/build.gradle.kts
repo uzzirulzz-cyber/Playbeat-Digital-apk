0d078ab70057d8709bf87c4dfa15029afa97323e
uzzirulzz-cyber <uzzirulzz@gmail.com>
2026-09-07 14:16:39 +0500

    brand: applicationId digital.playbeat.app + versionName 1.0.0 (matches storefront config)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,11 +14,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.playbeatdigital.pvqjzk"
+    applicationId = "digital.playbeat.app"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "1.0"
+    versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
